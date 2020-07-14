05cf1eee7c20fa1dd09a6533e3bf74916a9eac26
Serpantes <4uteppp@gmail.com>
2020-07-14 13:23:19 +0300

    Init
    TODO list

---
+++ main.go
@@ -0,0 +1,14 @@
+package auth
+
+import (
+	tgbotapi "gopkg.in/telegram-bot-api.v4"
+)
+
+func main() {
+	//todo twitter API  https://tutorialedge.net/golang/writing-a-twitter-bot-golang/
+	//todo tg api https://godoc.org/gopkg.in/telegram-bot-api.v4
+
+	//todo steam price Checker https://steamapis.com/docs/market#app
+	//todo PSstore prce Checker  https://github.com/Tustin/psn-go
+	//todo xbox price checker
+}
